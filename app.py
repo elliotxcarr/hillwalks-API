76d0ce9bc91dff33cad60e504b645b499a7df39b
Elliot Carr <Elliot.Carr@navtor.com>
2025-04-07 11:42:12 +0100

    Login endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
 
@@ -20,6 +20,26 @@ def get_walks():
 def get_users():
     users = list(usersCollection.find({}, {"_id":0}))
     return jsonify(users)
+
+@app.route('/login', methods=['POST'])
+def login():
+    
+    data = request.get_json()
+    
+    username = data.get('username')
+    password = data.get('password')
+
+    user = usersCollection.find_one({"username": username})
+    
+    if user and user.get("password") == password:
+        return jsonify({
+            "username": user["username"],
+            "email": user.get("email"),
+        }),200
+    
+    return jsonify({"error": "Invalid credentials"}),401
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
